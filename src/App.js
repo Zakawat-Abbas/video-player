c316e71739064286b3de612a3978ab72be790838
Zakawat Abbas <zakawatabbas123@gmail.com>
2024-04-23 15:46:46 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import VideoPlayer from './components/VideoPlayer';
+import 'video.js/dist/video-js.css';
 
-function App() {
+const App = () => {
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      type: 'video/mp4'
+    }]
+  };
+
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    const forwardButton = document.createElement('button');
+    forwardButton.textContent = 'Forward 10s';
+    forwardButton.className = 'vjs-button vjs-control';
+    forwardButton.addEventListener('click', () => {
+      player.currentTime(player.currentTime() + 10);
+    });
+
+    const backwardButton = document.createElement('button');
+    backwardButton.textContent = 'Backward 10s';
+    backwardButton.className = 'vjs-button vjs-control';
+    backwardButton.addEventListener('click', () => {
+      player.currentTime(player.currentTime() - 10);
+    });
+
+    const playerControlBar = player.controlBar.el();
+    playerControlBar.insertBefore(forwardButton, playerControlBar.firstChild);
+    playerControlBar.insertBefore(backwardButton, playerControlBar.firstChild);
+
+
+    const playerRegion = player.el();
+    playerRegion.addEventListener('dblclick', (event) => {
+      event.preventDefault();
+      const clickPosition = event.offsetX / playerRegion.offsetWidth;
+      if (clickPosition < 0.33) {
+        player.currentTime(player.currentTime() - 10);
+      } else if (clickPosition > 0.66) {
+        player.currentTime(player.currentTime() + 10);
+      }
+    });
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Rest of app here</div>
+      <VideoPlayer options={videoJsOptions} onReady={handlePlayerReady} width={640} height={360} />
+      <div>Rest of app here</div>
+    </>
   );
 }
 
